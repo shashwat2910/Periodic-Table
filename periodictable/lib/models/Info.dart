b319d309c9f8e9f75445bc47c9289285ceec15cd
Shashwat Sharma <shashwat2910@gmail.com>
2021-01-23 16:23:08 +0530

    basic idea

--- periodictable/lib/models/Info.dart
+++ periodictable/lib/models/Info.dart
@@ -60,59 +60,18 @@ class Info extends StatelessWidget {
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 300,
                     decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white),
-                      color: Color(color),
-                    ),
-                    height: 120,
-                    width: 100,
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8)),
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          atomNumber,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          symbol,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          name,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 13,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 1,
-                        ),
-                        Text(
-                          group,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 10,
-                          ),
-                        )
-                      ],
+                      children: [],
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
