e2a200166c438fd931cd26ff801a2981404953fd
Shashwat Sharma <shashwat2910@gmail.com>
2021-01-24 19:23:21 +0530

    period2 info completed

---
+++ periodictable/lib/Information/period2.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:periodictable/models/Info.dart';
+
+class Lithium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "3",
+      symbol: "Li",
+      name: "Lithium",
+      group: "Alkali Metal",
+      color: 0xFFFFC3C3,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 7.0 u",
+      electronConfig: "Electron Configuration: [He]2s1",
+      oxidationStates: "Oxidation States: +1",
+      electronegativity: "Electronegativity: 0.98",
+      atomicRadius: "Atomic Radius: 182 pm",
+      ionizationEnergy: "Ionization Energy: 5.392 eV",
+      electronAffinity: "Electron Affinity: 0.618 eV",
+      meltingPoint: "Melting Point: 453.65 K",
+      boilingPoint: "Boiling Point: 1615 K",
+      density: "Density: 0.534 g/cm³",
+      year: "Year Discovered: 1817",
+    );
+  }
+}
+
+class Beryllium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "4",
+      symbol: "Be",
+      name: "Beryllium",
+      group: "Alkali Earth Metal",
+      color: 0xFFD2D2FF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 9.012183 u",
+      electronConfig: "Electron Configuration: [He]2s2",
+      oxidationStates: "Oxidation States: +2",
+      electronegativity: "Electronegativity: 1.57",
+      atomicRadius: "Atomic Radius: 153 pm",
+      ionizationEnergy: "Ionization Energy: 9.323 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1560 K",
+      boilingPoint: "Boiling Point: 2744 K",
+      density: "Density: 1.85 g/cm³",
+      year: "Year Discovered: 1798",
+    );
+  }
+}
+
+class Boron extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "5",
+      symbol: "B",
+      name: "Boron",
+      group: "Metalloid",
+      color: 0xFFE1EEBB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 10.81 u",
+      electronConfig: "Electron Configuration: [He]2s22p1",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 2.04",
+      atomicRadius: "Atomic Radius: 192 pm",
+      ionizationEnergy: "Ionization Energy: 8.298 eV",
+      electronAffinity: "Electron Affinity: 0.277 eV",
+      meltingPoint: "Melting Point: 2348 K",
+      boilingPoint: "Boiling Point: 4273 K",
+      density: "Density: 2.37 g/cm³",
+      year: "Year Discovered: 1808",
+    );
+  }
+}
+
+class Carbon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "6",
+      symbol: "C",
+      name: "Carbon",
+      group: "NonMetal",
+      color: 0xFFFFFFBB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 12.011 u",
+      electronConfig: "Electron Configuration: [He]2s22p2",
+      oxidationStates: "Oxidation States: +4, +2, -4",
+      electronegativity: "Electronegativity: 2.55",
+      atomicRadius: "Atomic Radius: 170 pm",
+      ionizationEnergy: "Ionization Energy: 11.260 eV",
+      electronAffinity: "Electron Affinity: 1.263 eV",
+      meltingPoint: "Melting Point: 3823 K",
+      boilingPoint: "Boiling Point: 4098 K",
+      density: "Density: 2.2670 g/cm³",
+      year: "Year Discovered: Ancient",
+    );
+  }
+}
+
+class Nitrogen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "7",
+      symbol: "N",
+      name: "Nitrogen",
+      group: "NonMetal",
+      color: 0xFFFFFFBB,
+      state: "Standard State: Gas",
+      atomMass: "Atomic Mass: 14.007 u",
+      electronConfig: "Electron Configuration: [He]2s22p3",
+      oxidationStates: "Oxidation States: +5, +4, +3, +2, +1, -1, -2, -3",
+      electronegativity: "Electronegativity: 3.04",
+      atomicRadius: "Atomic Radius: 155 pm",
+      ionizationEnergy: "Ionization Energy: 14.534 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 63.15 K",
+      boilingPoint: "Boiling Point: 77.36 K",
+      density: "Density: 0.0012506 g/cm³",
+      year: "Year Discovered: 1772",
+    );
+  }
+}
+
+class Oxygen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "8",
+      symbol: "O",
+      name: "Oxygen",
+      group: "NonMetal",
+      color: 0xFFFFFFBB,
+      state: "Standard State: Gas",
+      atomMass: "Atomic Mass: 15.999 u",
+      electronConfig: "Electron Configuration: [He]2s22p4",
+      oxidationStates: "Oxidation States: -2",
+      electronegativity: "Electronegativity: 3.44",
+      atomicRadius: "Atomic Radius: 152 pm",
+      ionizationEnergy: "Ionization Energy: 13.618 eV",
+      electronAffinity: "Electron Affinity: 1.461 eV",
+      meltingPoint: "Melting Point: 54.36 K",
+      boilingPoint: "Boiling Point: 90.2 K",
+      density: "Density: 0.001429 g/cm³",
+      year: "Year Discovered: 1774",
+    );
+  }
+}
+
+class Fluorine extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "9",
+      symbol: "F",
+      name: "Fluorine",
+      group: "NonMetal",
+      color: 0xFFFFFFBB,
+      state: "Standard State: Gas",
+      atomMass: "Atomic Mass: 18.99840316 u",
+      electronConfig: "Electron Configuration: [He]2s22p5",
+      oxidationStates: "Oxidation States: -1",
+      electronegativity: "Electronegativity: 3.98",
+      atomicRadius: "Atomic Radius: 135 pm",
+      ionizationEnergy: "Ionization Energy: 17.423 eV",
+      electronAffinity: "Electron Affinity: 3.339 eV",
+      meltingPoint: "Melting Point: 53.53 K",
+      boilingPoint: "Boiling Point: 85.03 K",
+      density: "Density: 0.001696 g/cm³",
+      year: "Year Discovered: 1670",
+    );
+  }
+}
+
+class Neon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "10",
+      symbol: "Ne",
+      name: "Neon",
+      group: "Gas",
+      color: 0xFFFFE3BA,
+      state: "Standard State: Gas",
+      atomMass: "Atomic Mass: 20.180 u",
+      electronConfig: "Electron Configuration: [He]2s22p6",
+      oxidationStates: "Oxidation States: 0",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: 154 pm",
+      ionizationEnergy: "Ionization Energy: 21.565 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 24.56 K",
+      boilingPoint: "Boiling Point: 27.07 K",
+      density: "Density: 0.0008999 g/cm³",
+      year: "Year Discovered: 1898",
+    );
+  }
+}
--- periodictable/lib/models/period2.dart
+++ periodictable/lib/models/period2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:periodictable/Information/period2.dart';
 
 import 'block.dart';
 
@@ -11,43 +12,39 @@ class Period2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        ElementTile(
-          atomnum: "3",
-          symbol: "Li",
-          name: "Lithium",
-          group: "AlkaliMetal",
-          colorTile: 0xFFFFC3C3,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Lithium()));
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "3",
+              symbol: "Li",
+              name: "Lithium",
+              group: "AlkaliMetal",
+              colorTile: 0xFFFFC3C3,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "4",
-          symbol: "Be",
-          name: "Beryllium",
-          group: "AlkalineEarth",
-          colorTile: 0xFFD2D2FF,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Beryllium()));
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "4",
+              symbol: "Be",
+              name: "Beryllium",
+              group: "AlkalineEarth",
+              colorTile: 0xFFD2D2FF,
+            ),
+          ),
         ),
-        SizedBox(
-          width: 3.5,
-        ),
-        EmptyTile(),
-        SizedBox(
-          width: 3.5,
-        ),
-        EmptyTile(),
-        SizedBox(
-          width: 3.5,
-        ),
-        EmptyTile(),
-        SizedBox(
-          width: 3.5,
-        ),
-        EmptyTile(),
-        SizedBox(
-          width: 3.5,
-        ),
-        EmptyTile(),
         SizedBox(
           width: 3.5,
         ),
@@ -71,62 +68,130 @@ class Period2 extends StatelessWidget {
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "5",
-          symbol: "B",
-          name: "Boron",
-          group: "Metalloid",
-          colorTile: 0xFFE1EEBB,
+        EmptyTile(),
+        SizedBox(
+          width: 3.5,
+        ),
+        EmptyTile(),
+        SizedBox(
+          width: 3.5,
+        ),
+        EmptyTile(),
+        SizedBox(
+          width: 3.5,
+        ),
+        EmptyTile(),
+        SizedBox(
+          width: 3.5,
+        ),
+        EmptyTile(),
+        SizedBox(
+          width: 3.5,
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Boron()));
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "5",
+              symbol: "B",
+              name: "Boron",
+              group: "Metalloid",
+              colorTile: 0xFFE1EEBB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "6",
-          symbol: "C",
-          name: "Carbon",
-          group: "NonMetal",
-          colorTile: 0xFFFFFFBB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Carbon()));
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "6",
+              symbol: "C",
+              name: "Carbon",
+              group: "NonMetal",
+              colorTile: 0xFFFFFFBB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "7",
-          symbol: "N",
-          name: "Nitrogen",
-          group: "NonMetal",
-          colorTile: 0xFFFFFFBB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Nitrogen()));
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "7",
+              symbol: "N",
+              name: "Nitrogen",
+              group: "NonMetal",
+              colorTile: 0xFFFFFFBB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "8",
-          symbol: "O",
-          name: "Oxygen",
-          group: "NonMetal",
-          colorTile: 0xFFFFFFBB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Oxygen()));
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "8",
+              symbol: "O",
+              name: "Oxygen",
+              group: "NonMetal",
+              colorTile: 0xFFFFFFBB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "9",
-          symbol: "F",
-          name: "Fluorine",
-          group: "Halogen",
-          colorTile: 0xFFFFFFBC,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Fluorine()));
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "9",
+              symbol: "F",
+              name: "Fluorine",
+              group: "Halogen",
+              colorTile: 0xFFFFFFBC,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "10",
-          symbol: "Ne",
-          name: "Neon",
-          group: "NobleGas",
-          colorTile: 0xFFFFE3BA,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Neon()));
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "10",
+              symbol: "Ne",
+              name: "Neon",
+              group: "NobleGas",
+              colorTile: 0xFFFFE3BA,
+            ),
+          ),
         ),
       ],
     );
