20294095f84c5fd13d379ed888c458ca3c699d0c
Shashwat Sharma <shashwat2910@gmail.com>
2021-01-27 11:41:16 +0530

    period7 done

---
+++ periodictable/lib/Information/period7.dart
@@ -0,0 +1,427 @@
+import 'package:flutter/material.dart';
+import 'package:periodictable/models/Info.dart';
+
+class Francium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "87",
+      symbol: "Fr",
+      name: "Francium",
+      group: "Alkali Metal",
+      color: 0xFFFFC3C3,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 223.01 u",
+      electronConfig: "Electron Configuration: [Rn]7s1",
+      oxidationStates: "Oxidation States: +1",
+      electronegativity: "Electronegativity: 0.7",
+      atomicRadius: "Atomic Radius: 348 pm",
+      ionizationEnergy: "Ionization Energy: 3.9 eV",
+      electronAffinity: "Electron Affinity: 0.47 eV",
+      meltingPoint: "Melting Point: 300 K",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1939",
+    );
+  }
+}
+
+class Radium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "88",
+      symbol: "Ra",
+      name: "Radium",
+      group: "Alkali Earth Metal",
+      color: 0xFFD2D2FF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 226.02 u",
+      electronConfig: "Electron Configuration: [Rn]7s2",
+      oxidationStates: "Oxidation States: +2",
+      electronegativity: "Electronegativity: 0.9",
+      atomicRadius: "Atomic Radius: 283 pm",
+      ionizationEnergy: "Ionization Energy: 5.279 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 973 K",
+      boilingPoint: "Boiling Point: 1413 K",
+      density: "Density: 5 g/cm³",
+      year: "Year Discovered: 1898",
+    );
+  }
+}
+
+class Rutherfordium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "104",
+      symbol: "Rf",
+      name: "Rutherfordium",
+      group: "Transition Metal",
+      color: 0xFFB8DCFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 267.12 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f146d2",
+      oxidationStates: "Oxidation States: +4",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1964",
+    );
+  }
+}
+
+class Dubnium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "105",
+      symbol: "Db",
+      name: "Dubnium",
+      group: "Transition Metal",
+      color: 0xFFB8DCFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 268.12 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f146d3",
+      oxidationStates: "Oxidation States: +5, +4, +3",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1967",
+    );
+  }
+}
+
+class Seaborgium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "106",
+      symbol: "Sg",
+      name: "Seaborgium",
+      group: "Transition Metal",
+      color: 0xFFB8DCFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 271.13 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f146d4",
+      oxidationStates: "Oxidation States: +6, +5, +4, +3, 0",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1783",
+    );
+  }
+}
+
+class Bohrium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "107",
+      symbol: "Bh",
+      name: "Bohrium",
+      group: "Transition Metal",
+      color: 0xFFB8DCFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 274.14 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f146d5",
+      oxidationStates: "Oxidation States: +7, +6, +5, +4, +3",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1976",
+    );
+  }
+}
+
+class Hassium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "108",
+      symbol: "Hs",
+      name: "Hassium",
+      group: "Transition Metal",
+      color: 0xFFB8DCFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 277.15 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f146d6",
+      oxidationStates: "Oxidation States: +8, +6, +5, +4, +3, +2",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1984",
+    );
+  }
+}
+
+class Meitnerium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "109",
+      symbol: "Mt",
+      name: "Meitnerium",
+      group: "Transition Metal",
+      color: 0xFFB8DCFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 278.15 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f146d7",
+      oxidationStates: "Oxidation States: +9, +8, +6, +4, +3, +1",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1982",
+    );
+  }
+}
+
+class Darmstadtium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "110",
+      symbol: "Ds",
+      name: "Darmstadtium",
+      group: "Transition Metal",
+      color: 0xFFB8DCFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 281.16 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f146d8",
+      oxidationStates: "Oxidation States: +8, +6, +4, +2, 0",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1994",
+    );
+  }
+}
+
+class Roentgenium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "111",
+      symbol: "Rg",
+      name: "Roentgenium",
+      group: "Transition Metal",
+      color: 0xFFB8DCFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 282.16 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f146d9",
+      oxidationStates: "Oxidation States: +5, +3, +1, -1",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1994",
+    );
+  }
+}
+
+class Copernicium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "112",
+      symbol: "Cn",
+      name: "Copernicium",
+      group: "Transition Metal",
+      color: 0xFFB8DCFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 285.17 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f146d10",
+      oxidationStates: "Oxidation States: +2, +1, 0",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1996",
+    );
+  }
+}
+
+class Nihonium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "113",
+      symbol: "Nh",
+      name: "Nihonium",
+      group: "Post-Transition Metal",
+      color: 0xFFBBFFBB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 286.18 u",
+      electronConfig: "Electron Configuration: [Rn]5f146d107s27p1",
+      oxidationStates: "Oxidation States: Null",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 2004",
+    );
+  }
+}
+
+class Flerovium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "114",
+      symbol: "Fl",
+      name: "Flerovium",
+      group: "Post-Transition Metal",
+      color: 0xFFBBFFBB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 289.19 u",
+      electronConfig: "Electron Configuration: [Rn]7s27p25f146d10",
+      oxidationStates: "Oxidation States: +6, +4, +2, +1, 0",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1998",
+    );
+  }
+}
+
+class Moscovium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "115",
+      symbol: "Mc",
+      name: "Moscovium",
+      group: "Post-Transition Metal",
+      color: 0xFFBBFFBB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 290.19 u",
+      electronConfig: "Electron Configuration: [Rn]7s27p35f146d10",
+      oxidationStates: "Oxidation States: +3, +1",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 2003",
+    );
+  }
+}
+
+class Livermorium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "116",
+      symbol: "Lv",
+      name: "Livermorium",
+      group: "Post-Transition Metal",
+      color: 0xFFBBFFBB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 293.20 u",
+      electronConfig: "Electron Configuration: [Rn]7s27p45f146d10",
+      oxidationStates: "Oxidation States: +4, +2, -2",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 2000",
+    );
+  }
+}
+
+class Tennessine extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "117",
+      symbol: "Ts",
+      name: "Tennessine",
+      group: "Halogen",
+      color: 0xFFFFFFB9,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 294.21 u",
+      electronConfig: "Electron Configuration: [Rn]7s27p55f146d10",
+      oxidationStates: "Oxidation States: +5, +3, +1, -1",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 2010",
+    );
+  }
+}
+
+class Oganesson extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "118",
+      symbol: "Og",
+      name: "Oganesson",
+      group: "Noble Gas",
+      color: 0xFFFFE3BA,
+      state: "Standard State: Gas",
+      atomMass: "Atomic Mass: 294.21 u",
+      electronConfig: "Electron Configuration: [Rn]7s27p65f146d10",
+      oxidationStates: "Oxidation States: +6, +4, +2, +1, 0, -1",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: Null",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 2006",
+    );
+  }
+}
--- periodictable/lib/models/period7.dart
+++ periodictable/lib/models/period7.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:periodictable/Information/period7.dart';
 
 import 'block.dart';
 
@@ -7,22 +8,46 @@ class Period7 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        ElementTile(
-          atomnum: "87",
-          symbol: "Fr",
-          name: "Francium",
-          group: "AlkaliMetal",
-          colorTile: 0xFFFFC3C3,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Francium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "87",
+              symbol: "Fr",
+              name: "Francium",
+              group: "AlkaliMetal",
+              colorTile: 0xFFFFC3C3,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "88",
-          symbol: "Ra",
-          name: "Radium",
-          group: "AlkalineEarth",
-          colorTile: 0xFFD2D2FF,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Radium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "88",
+              symbol: "Ra",
+              name: "Radium",
+              group: "AlkalineEarth",
+              colorTile: 0xFFD2D2FF,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
@@ -33,152 +58,332 @@ class Period7 extends StatelessWidget {
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "104",
-          symbol: "Rf",
-          name: "Rutherfordium",
-          group: "TransitionMetal",
-          colorTile: 0xFFB8DCFF,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Rutherfordium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "104",
+              symbol: "Rf",
+              name: "Rutherfordium",
+              group: "TransitionMetal",
+              colorTile: 0xFFB8DCFF,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "105",
-          symbol: "Db",
-          name: "Dubnium",
-          group: "TransitionMetal",
-          colorTile: 0xFFB8DCFF,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Dubnium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "105",
+              symbol: "Db",
+              name: "Dubnium",
+              group: "TransitionMetal",
+              colorTile: 0xFFB8DCFF,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "106",
-          symbol: "Sg",
-          name: "Seaborgium",
-          group: "TransitionMetal",
-          colorTile: 0xFFB8DCFF,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Seaborgium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "106",
+              symbol: "Sg",
+              name: "Seaborgium",
+              group: "TransitionMetal",
+              colorTile: 0xFFB8DCFF,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "107",
-          symbol: "Bh",
-          name: "Bohrium",
-          group: "TransitionMetal",
-          colorTile: 0xFFB8DCFF,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Bohrium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "107",
+              symbol: "Bh",
+              name: "Bohrium",
+              group: "TransitionMetal",
+              colorTile: 0xFFB8DCFF,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "108",
-          symbol: "Hs",
-          name: "Hassium",
-          group: "TransitionMetal",
-          colorTile: 0xFFB8DCFF,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Hassium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "108",
+              symbol: "Hs",
+              name: "Hassium",
+              group: "TransitionMetal",
+              colorTile: 0xFFB8DCFF,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "109",
-          symbol: "Mt",
-          name: "Meitnerium",
-          group: "TransitionMetal",
-          colorTile: 0xFFB8DCFF,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Meitnerium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "109",
+              symbol: "Mt",
+              name: "Meitnerium",
+              group: "TransitionMetal",
+              colorTile: 0xFFB8DCFF,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "110",
-          symbol: "Ds",
-          name: "Darmstadtium",
-          group: "TransitionMetal",
-          colorTile: 0xFFB8DCFF,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Darmstadtium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "110",
+              symbol: "Ds",
+              name: "Darmstadtium",
+              group: "TransitionMetal",
+              colorTile: 0xFFB8DCFF,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "111",
-          symbol: "Rg",
-          name: "Roentgenium",
-          group: "TransitionMetal",
-          colorTile: 0xFFB8DCFF,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Roentgenium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "111",
+              symbol: "Rg",
+              name: "Roentgenium",
+              group: "TransitionMetal",
+              colorTile: 0xFFB8DCFF,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "112",
-          symbol: "Cn",
-          name: "Copernicium",
-          group: "TransitionMetal",
-          colorTile: 0xFFB8DCFF,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Copernicium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "112",
+              symbol: "Cn",
+              name: "Copernicium",
+              group: "TransitionMetal",
+              colorTile: 0xFFB8DCFF,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "113",
-          symbol: "Nh",
-          name: "Nihonium",
-          group: "PostTransition",
-          colorTile: 0xFFBBFFBB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Nihonium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "113",
+              symbol: "Nh",
+              name: "Nihonium",
+              group: "PostTransition",
+              colorTile: 0xFFBBFFBB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "114",
-          symbol: "Fl",
-          name: "Flerovium",
-          group: "PostTransition",
-          colorTile: 0xFFBBFFBB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Flerovium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "114",
+              symbol: "Fl",
+              name: "Flerovium",
+              group: "PostTransition",
+              colorTile: 0xFFBBFFBB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "115",
-          symbol: "Mc",
-          name: "Moscovium",
-          group: "PostTransition",
-          colorTile: 0xFFBBFFBB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Moscovium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "115",
+              symbol: "Mc",
+              name: "Moscovium",
+              group: "PostTransition",
+              colorTile: 0xFFBBFFBB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "116",
-          symbol: "Lv",
-          name: "Livermorium",
-          group: "PostTransition",
-          colorTile: 0xFFBBFFBB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Livermorium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "116",
+              symbol: "Lv",
+              name: "Livermorium",
+              group: "PostTransition",
+              colorTile: 0xFFBBFFBB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "117",
-          symbol: "Ts",
-          name: "Tennessine",
-          group: "Halogen",
-          colorTile: 0xFFFFFFBC,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Tennessine(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "117",
+              symbol: "Ts",
+              name: "Tennessine",
+              group: "Halogen",
+              colorTile: 0xFFFFFFBC,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "118",
-          symbol: "Og",
-          name: "oganesson",
-          group: "NobleGas",
-          colorTile: 0xFFFFE3BA,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Oganesson(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "118",
+              symbol: "Og",
+              name: "Oganesson",
+              group: "NobleGas",
+              colorTile: 0xFFFFE3BA,
+            ),
+          ),
         ),
       ],
     );
