74ef7efdde12992f191b91b9d4519b7b5a89c857
Shashwat Sharma <shashwat2910@gmail.com>
2021-01-27 13:37:38 +0530

    lanthanoid done

---
+++ periodictable/lib/Information/lanthanoid.dart
@@ -0,0 +1,376 @@
+import 'package:flutter/material.dart';
+import 'package:periodictable/models/Info.dart';
+
+class Potassium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "57",
+      symbol: "La",
+      name: "Lanthanum",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 138.90 u",
+      electronConfig: "Electron Configuration: [Xe]6s25d1",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.1",
+      atomicRadius: "Atomic Radius: 240 pm",
+      ionizationEnergy: "Ionization Energy: 5.77 eV",
+      electronAffinity: "Electron Affinity: 0.5 eV",
+      meltingPoint: "Melting Point: 1191 K",
+      boilingPoint: "Boiling Point: 3737 K",
+      density: "Density: 6.15 g/cm³",
+      year: "Year Discovered: 1839",
+    );
+  }
+}
+
+class Cerium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "58",
+      symbol: "Ce",
+      name: "Cerium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 140.12 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f15d1",
+      oxidationStates: "Oxidation States: +4, +3",
+      electronegativity: "Electronegativity: 1.12",
+      atomicRadius: "Atomic Radius: 235 pm",
+      ionizationEnergy: "Ionization Energy: 5.539 eV",
+      electronAffinity: "Electron Affinity: 0.5 eV",
+      meltingPoint: "Melting Point: 1071 K",
+      boilingPoint: "Boiling Point: 3697 K",
+      density: "Density: 6.770 g/cm³",
+      year: "Year Discovered: 1803",
+    );
+  }
+}
+
+class Praseodymium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "59",
+      symbol: "Pr",
+      name: "Praseodymium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 140.90 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f3",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.13",
+      atomicRadius: "Atomic Radius: 239 pm",
+      ionizationEnergy: "Ionization Energy: 5.464 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1204 K",
+      boilingPoint: "Boiling Point: 3793 K",
+      density: "Density: 6.77 g/cm³",
+      year: "Year Discovered: 1885",
+    );
+  }
+}
+
+class Neodymium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "60",
+      symbol: "Nd",
+      name: "Neodymium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 144.24 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f4",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.14",
+      atomicRadius: "Atomic Radius: 229 pm",
+      ionizationEnergy: "Ionization Energy: 5.525 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1294 K",
+      boilingPoint: "Boiling Point: 3347 K",
+      density: "Density: 7.01 g/cm³",
+      year: "Year Discovered: 1885",
+    );
+  }
+}
+
+class Promethium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "61",
+      symbol: "Pm",
+      name: "Promethium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 144.91 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f5",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: 236 pm",
+      ionizationEnergy: "Ionization Energy: 5.55 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1315 K",
+      boilingPoint: "Boiling Point: 3273 K",
+      density: "Density: 7.26 g/cm³",
+      year: "Year Discovered: 1945",
+    );
+  }
+}
+
+class Samarium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "62",
+      symbol: "Sm",
+      name: "Samarium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 150.4 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f6",
+      oxidationStates: "Oxidation States: +3, +2",
+      electronegativity: "Electronegativity: 1.17",
+      atomicRadius: "Atomic Radius: 229 pm",
+      ionizationEnergy: "Ionization Energy: 5.644 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1347 K",
+      boilingPoint: "Boiling Point: 2067 K",
+      density: "Density: 7.52 g/cm³",
+      year: "Year Discovered: 1879",
+    );
+  }
+}
+
+class Europium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "63",
+      symbol: "Eu",
+      name: "Europium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 151.96 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f7",
+      oxidationStates: "Oxidation States: +3, +2",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: 233 pm",
+      ionizationEnergy: "Ionization Energy: 5.670 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1095 K",
+      boilingPoint: "Boiling Point: 1802 K",
+      density: "Density: 5.24 g/cm³",
+      year: "Year Discovered: 1901",
+    );
+  }
+}
+
+class Gadolinium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "64",
+      symbol: "Gd",
+      name: "Gadolinium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 157.2 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f75d1",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.2",
+      atomicRadius: "Atomic Radius: 237 pm",
+      ionizationEnergy: "Ionization Energy: 6.150 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1586 K",
+      boilingPoint: "Boiling Point: 3546 K",
+      density: "Density: 7.90 g/cm³",
+      year: "Year Discovered: 1880",
+    );
+  }
+}
+
+class Terbium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "65",
+      symbol: "Tb",
+      name: "Terbium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 158.92 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f9",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: 221 pm",
+      ionizationEnergy: "Ionization Energy: 5.864 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1629 K",
+      boilingPoint: "Boiling Point: 3503 K",
+      density: "Density: 8.23 g/cm³",
+      year: "Year Discovered: 1843",
+    );
+  }
+}
+
+class Dysprosium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "66",
+      symbol: "Dy",
+      name: "Dysprosium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 162.50 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f10",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.22",
+      atomicRadius: "Atomic Radius: 229 pm",
+      ionizationEnergy: "Ionization Energy: 5.939 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1685 K",
+      boilingPoint: "Boiling Point: 2840 K",
+      density: "Density: 8.55 g/cm³",
+      year: "Year Discovered: 1886",
+    );
+  }
+}
+
+class Holmium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "67",
+      symbol: "Ho",
+      name: "Holmium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 164.93 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f11",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.23",
+      atomicRadius: "Atomic Radius: 216 pm",
+      ionizationEnergy: "Ionization Energy: 6.022 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1747 K",
+      boilingPoint: "Boiling Point: 2973 K",
+      density: "Density: 8.80 g/cm³",
+      year: "Year Discovered: 1878",
+    );
+  }
+}
+
+class Erbium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "68",
+      symbol: "Er",
+      name: "Erbium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 167.26 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f12",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.24",
+      atomicRadius: "Atomic Radius: 235 pm",
+      ionizationEnergy: "Ionization Energy: 6.108 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1802 K",
+      boilingPoint: "Boiling Point: 3141 K",
+      density: "Density: 9.07 g/cm³",
+      year: "Year Discovered: 1843",
+    );
+  }
+}
+
+class Thulium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "69",
+      symbol: "Tm",
+      name: "Thulium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 168.93 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f13",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.25",
+      atomicRadius: "Atomic Radius: 227 pm",
+      ionizationEnergy: "Ionization Energy: 6.184 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1818 K",
+      boilingPoint: "Boiling Point: 2223 K",
+      density: "Density: 9.32 g/cm³",
+      year: "Year Discovered: 1879",
+    );
+  }
+}
+
+class Ytterbium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "70",
+      symbol: "Yb",
+      name: "Ytterbium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 173.04 u",
+      electronConfig: "Electron Configuration: [Xe]6s24f14",
+      oxidationStates: "Oxidation States: +3, +2",
+      electronegativity: "Electronegativity: Null",
+      atomicRadius: "Atomic Radius: 242 pm",
+      ionizationEnergy: "Ionization Energy: 6.254 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1092 K",
+      boilingPoint: "Boiling Point: 1469 K",
+      density: "Density: 6.90 g/cm³",
+      year: "Year Discovered: 1878",
+    );
+  }
+}
+
+class Lutetium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "71",
+      symbol: "Lu",
+      name: "Lutetium",
+      group: "Lanthanide",
+      color: 0xFFADFFFF,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 174.96 u",
+      electronConfig: "Electron Configuration: [Xe]6s25d1",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.27",
+      atomicRadius: "Atomic Radius: 221 pm",
+      ionizationEnergy: "Ionization Energy: 5.42 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1936 K",
+      boilingPoint: "Boiling Point: 3675 K",
+      density: "Density: 9.84 g/cm³",
+    );
+  }
+}
