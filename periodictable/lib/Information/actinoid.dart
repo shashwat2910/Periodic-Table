b78629c7fef232b4865ddd444763d2cb11b471df
Shashwat Sharma <shashwat2910@gmail.com>
2021-01-27 14:43:49 +0530

    project Completed

---
+++ periodictable/lib/Information/actinoid.dart
@@ -0,0 +1,377 @@
+import 'package:flutter/material.dart';
+import 'package:periodictable/models/Info.dart';
+
+class Actinium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "89",
+      symbol: "Ac",
+      name: "Actinium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 227.02 u",
+      electronConfig: "Electron Configuration: [Rn]7s26d1",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.1",
+      atomicRadius: "Atomic Radius: 260 pm",
+      ionizationEnergy: "Ionization Energy: 5.17 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1324 K",
+      boilingPoint: "Boiling Point: 3471 K",
+      density: "Density: 10.07 g/cm³",
+      year: "Year Discovered: 1899",
+    );
+  }
+}
+
+class Thorium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "90",
+      symbol: "Th",
+      name: "Thorium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 232.038 u",
+      electronConfig: "Electron Configuration: [Rn]7s26d2",
+      oxidationStates: "Oxidation States: +4",
+      electronegativity: "Electronegativity: 1.3",
+      atomicRadius: "Atomic Radius: 237 pm",
+      ionizationEnergy: "Ionization Energy: 6.08 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 2023 K",
+      boilingPoint: "Boiling Point: 5061 K",
+      density: "Density: 11.72 g/cm³",
+      year: "Year Discovered: 1828",
+    );
+  }
+}
+
+class Protactinium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "91",
+      symbol: "Pa",
+      name: "Protactinium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 231.03 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f26d1",
+      oxidationStates: "Oxidation States: +5, +4",
+      electronegativity: "Electronegativity: 1.5",
+      atomicRadius: "Atomic Radius: 243 pm",
+      ionizationEnergy: "Ionization Energy: 5.89 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1845 K",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: 15.37 g/cm³",
+      year: "Year Discovered: 1913",
+    );
+  }
+}
+
+class Uranium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "92",
+      symbol: "U",
+      name: "Uranium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 238.02 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f36d1",
+      oxidationStates: "Oxidation States: +6, +5, +4, +3",
+      electronegativity: "Electronegativity: 1.38",
+      atomicRadius: "Atomic Radius: 240 pm",
+      ionizationEnergy: "Ionization Energy: 6.194 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1408 K",
+      boilingPoint: "Boiling Point: 4404 K",
+      density: "Density: 18.95 g/cm³",
+      year: "Year Discovered: 1789",
+    );
+  }
+}
+
+class Neptunium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "93",
+      symbol: "Np",
+      name: "Neptunium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 237.04 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f46d1",
+      oxidationStates: "Oxidation States: +6, +5, +4, +3",
+      electronegativity: "Electronegativity: 1.36",
+      atomicRadius: "Atomic Radius: 221 pm",
+      ionizationEnergy: "Ionization Energy: 6.266 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 917 K",
+      boilingPoint: "Boiling Point: 4175 K",
+      density: "Density: 20.25 g/cm³",
+      year: "Year Discovered: 1940",
+    );
+  }
+}
+
+class Plutonium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "94",
+      symbol: "Pu",
+      name: "Plutonium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 244.06 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f6",
+      oxidationStates: "Oxidation States: +6, +5, +4, +3",
+      electronegativity: "Electronegativity: 1.28",
+      atomicRadius: "Atomic Radius: 243 pm",
+      ionizationEnergy: "Ionization Energy: 6.06 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 913 K",
+      boilingPoint: "Boiling Point: 3501 K",
+      density: "Density: 19.84 g/cm³",
+      year: "Year Discovered: 1940",
+    );
+  }
+}
+
+class Americium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "95",
+      symbol: "Am",
+      name: "Americium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 243.06 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f7",
+      oxidationStates: "Oxidation States: +6, +5, +4, +3",
+      electronegativity: "Electronegativity: 1.3",
+      atomicRadius: "Atomic Radius: 244 pm",
+      ionizationEnergy: "Ionization Energy: 5.993 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1449 K",
+      boilingPoint: "Boiling Point: 2284 K",
+      density: "Density: 13.69 g/cm³",
+      year: "Year Discovered: 1944",
+    );
+  }
+}
+
+class Curium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "96",
+      symbol: "Cm",
+      name: "Curium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 247.04 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f76d1",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.3",
+      atomicRadius: "Atomic Radius: 245 pm",
+      ionizationEnergy: "Ionization Energy: 6.02 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1618 K",
+      boilingPoint: "Boiling Point: 3400 K",
+      density: "Density: 13.51 g/cm³",
+      year: "Year Discovered: 1944",
+    );
+  }
+}
+
+class Berkelium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "96",
+      symbol: "Cm",
+      name: "Berkelium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 247.07 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f9",
+      oxidationStates: "Oxidation States: +4, +3",
+      electronegativity: "Electronegativity: 1.3",
+      atomicRadius: "Atomic Radius: 244 pm",
+      ionizationEnergy: "Ionization Energy: 6.23 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1323 K",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: 14 g/cm³",
+      year: "Year Discovered: 1949",
+    );
+  }
+}
+
+class Californium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "98",
+      symbol: "Cf",
+      name: "Californium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 251.07 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f10",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.3",
+      atomicRadius: "Atomic Radius: 245 pm",
+      ionizationEnergy: "Ionization Energy: 6.30 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1173 K",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1950",
+    );
+  }
+}
+
+class Einsteinium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "99",
+      symbol: "Es",
+      name: "Einsteinium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 252.08 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f11",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.3",
+      atomicRadius: "Atomic Radius: 245 pm",
+      ionizationEnergy: "Ionization Energy: 6.42 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1133 K",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1952",
+    );
+  }
+}
+
+class Fermium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "100",
+      symbol: "Fm",
+      name: "Fermium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 257.09 u",
+      electronConfig: "Electron Configuration: [Rn]5f127s2",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.3",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: 6.50 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1800 K",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1952",
+    );
+  }
+}
+
+class Mendelevium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "101",
+      symbol: "Md",
+      name: "Mendelevium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 258.09 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f13",
+      oxidationStates: "Oxidation States: +3, +2",
+      electronegativity: "Electronegativity: 1.3",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: 6.58 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1100 K",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1955",
+    );
+  }
+}
+
+class Nobelium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "102",
+      symbol: "No",
+      name: "Nobelium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 259.10 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f14",
+      oxidationStates: "Oxidation States: +3, +2",
+      electronegativity: "Electronegativity: 1.3",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: 6.65 eV",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1100 K",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1957",
+    );
+  }
+}
+
+class Lawrencium extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Info(
+      atomNumber: "103",
+      symbol: "Lr",
+      name: "Lawrencium",
+      group: "Actinide",
+      color: 0xFFC2FFEB,
+      state: "Standard State: Solid",
+      atomMass: "Atomic Mass: 262.11 u",
+      electronConfig: "Electron Configuration: [Rn]7s25f146d1",
+      oxidationStates: "Oxidation States: +3",
+      electronegativity: "Electronegativity: 1.3",
+      atomicRadius: "Atomic Radius: Null",
+      ionizationEnergy: "Ionization Energy: Null",
+      electronAffinity: "Electron Affinity: Null",
+      meltingPoint: "Melting Point: 1900 K",
+      boilingPoint: "Boiling Point: Null",
+      density: "Density: Null",
+      year: "Year Discovered: 1961",
+    );
+  }
+}
--- periodictable/lib/models/actinoid.dart
+++ periodictable/lib/models/actinoid.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:periodictable/Information/actinoid.dart';
 import 'block.dart';
 
 class Actinoid extends StatelessWidget {
@@ -21,152 +21,332 @@ class Actinoid extends StatelessWidget {
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "89",
-          symbol: "Ac",
-          name: "Actinium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Actinium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "89",
+              symbol: "Ac",
+              name: "Actinium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "90",
-          symbol: "Th",
-          name: "Thorium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Thorium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "90",
+              symbol: "Th",
+              name: "Thorium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "91",
-          symbol: "Pa",
-          name: "Protactinium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Protactinium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "91",
+              symbol: "Pa",
+              name: "Protactinium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "92",
-          symbol: "U",
-          name: "Uranium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Uranium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "92",
+              symbol: "U",
+              name: "Uranium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "93",
-          symbol: "Np",
-          name: "Neptunium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Neptunium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "93",
+              symbol: "Np",
+              name: "Neptunium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "94",
-          symbol: "Pu",
-          name: "Plutonium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Plutonium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "94",
+              symbol: "Pu",
+              name: "Plutonium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "95",
-          symbol: "Am",
-          name: "Americium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Americium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "95",
+              symbol: "Am",
+              name: "Americium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "96",
-          symbol: "Cm",
-          name: "Curium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Curium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "96",
+              symbol: "Cm",
+              name: "Curium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "97",
-          symbol: "Bk",
-          name: "Berkelium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Berkelium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "97",
+              symbol: "Bk",
+              name: "Berkelium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "98",
-          symbol: "Cf",
-          name: "Californium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Californium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "98",
+              symbol: "Cf",
+              name: "Californium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "99",
-          symbol: "Es",
-          name: "Einsteinium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Einsteinium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "99",
+              symbol: "Es",
+              name: "Einsteinium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "100",
-          symbol: "Fm",
-          name: "Fermium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Fermium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "100",
+              symbol: "Fm",
+              name: "Fermium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "101",
-          symbol: "Md",
-          name: "Mendelevium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Mendelevium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "101",
+              symbol: "Md",
+              name: "Mendelevium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "102",
-          symbol: "No",
-          name: "Nobelium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Nobelium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "102",
+              symbol: "No",
+              name: "Nobelium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
         SizedBox(
           width: 3.5,
         ),
-        ElementTile(
-          atomnum: "103",
-          symbol: "Lr",
-          name: "Lawrencium",
-          group: "Actinide",
-          colorTile: 0xFFC2FFEB,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Lawrencium(),
+              ),
+            );
+          },
+          child: Container(
+            child: ElementTile(
+              atomnum: "103",
+              symbol: "Lr",
+              name: "Lawrencium",
+              group: "Actinide",
+              colorTile: 0xFFC2FFEB,
+            ),
+          ),
         ),
       ],
     );
